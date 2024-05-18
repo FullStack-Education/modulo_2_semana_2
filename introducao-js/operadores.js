39a65dc3b594d96245af6efabf4e8387fe95ad1c
Julia-Barbosa <c0683880@vale.com>
2024-05-17 22:06:51 -0300

    aula 4 - atividade pratica

--- introducao-js/operadores.js
+++ introducao-js/operadores.js
@@ -2,37 +2,37 @@
 let numero_1 = 20;
 let numero_2 = 12;
 
-console.log("soma", numero_1 + numero_2);
-console.log("subtrair", numero_1 - numero_2);
-console.log("multiplicar", numero_1 * numero_2);
-console.log("dividir", numero_1 / numero_2);
-console.log("resto", numero_1 % numero_2);
-console.log("expo", numero_1 ** numero_2);
+// console.log("soma", numero_1 + numero_2);
+// console.log("subtrair", numero_1 - numero_2);
+// console.log("multiplicar", numero_1 * numero_2);
+// console.log("dividir", numero_1 / numero_2);
+// console.log("resto", numero_1 % numero_2);
+// console.log("expo", numero_1 ** numero_2);
 
 //operadores de atribuicao
-let numero_3 = 40;
-let numero_4 = 20;
-console.log( numero_3 += numero_4); // 40 + 20 = 60
-console.log( numero_3 -= numero_4); //60 - 20 = 40
-console.log( numero_3 *= numero_4);  //40*20 = 800
-console.log( numero_3 **= numero_4); //800**20 = longoooo
-console.log( numero_3 /= numero_4); //longooo
-console.log( numero_3 %= numero_4); //12
+// let numero_3 = 40;
+// let numero_4 = 20;
+// console.log( numero_3 += numero_4); // 40 + 20 = 60
+// console.log( numero_3 -= numero_4); //60 - 20 = 40
+// console.log( numero_3 *= numero_4);  //40*20 = 800
+// console.log( numero_3 **= numero_4); //800**20 = longoooo
+// console.log( numero_3 /= numero_4); //longooo
+// console.log( numero_3 %= numero_4); //12
 
-console.log( numero_3 = numero_3 + numero_4); 
+// console.log( numero_3 = numero_3 + numero_4); 
 
 //operadores de comparacao
 let numero_5 = 2;
 let numero_6 = 1;
 
-console.log(numero_6 == numero_5); //verifica o valor
-console.log(numero_6 === numero_5); //verifica o valor E o tipo do dado
-console.log(numero_6 > numero_5); 
-console.log(numero_6 < numero_5); 
-console.log(numero_6 >= numero_5); 
-console.log(numero_6 <= numero_5);
-console.log(numero_6 != numero_5); 
-console.log(numero_6 !== numero_5);
+// console.log(numero_6 == numero_5); //verifica o valor
+// console.log(numero_6 === numero_5); //verifica o valor E o tipo do dado
+// console.log(numero_6 > numero_5); 
+// console.log(numero_6 < numero_5); 
+// console.log(numero_6 >= numero_5); 
+// console.log(numero_6 <= numero_5);
+// console.log(numero_6 != numero_5); 
+// console.log(numero_6 !== numero_5);
 
 //operadores logicos
 let numero_7 = 2;
@@ -42,10 +42,23 @@ let numero_8 = 3;
 // || = OU - true || false = true;
 // ! = NEGACAO -> NOT - !true = false;
 
-console.log(numero_6 !== numero_5);
-console.log(numero_6 || numero_5);
-console.log(!numero_6);
+
+let fruta_1 = true;
+let fruta_2 = true;
+
+console.log(fruta_1 && fruta_2); //true e true
+console.log(fruta_1 || fruta_2);
+console.log(!fruta_1);
+// console.log(numero_6 || numero_5);
+// console.log(!numero_6);
 
 //operadores unarios
-console.log(numero_8++); //incrementando +1
-console.log(numero_8--); //decrementando -1
+
+// numero_8++;
+console.log(++numero_8); //incrementando +1
+
+console.log(--numero_8);
+// numero_8--;
+console.log(numero_8); //decrementando -1
+
+
